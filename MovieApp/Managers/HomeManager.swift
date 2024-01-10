270a9d6660c56c9fa7d3b182878a4dfb3af5b813
Ziyadkhan Taghiyev <ziyadxan.tagiyev@gmail.com>
2024-01-10 20:19:14 +0400

    Commit #4

--- MovieApp/Managers/HomeManager.swift
+++ MovieApp/Managers/HomeManager.swift
@@ -24,38 +24,4 @@ class HomeManager: MovieListUseCase {
 //            completion: completion)
 //    }
 
-
-//}
-
-//class HomeManager: MovieListUseCase {
-//    func getMovieList(pageNumber: Int, endpoint: HomeEndpoint, completion: @escaping ((Movie?, String?) -> Void)) {
-//        NetworkManager.request(
-//            model: Movie.self,
-//            endpoint: endpoint.rawValue,
-//            completion: completion)
-//    }
-
-
-//}
-
-//class HomeManager: MovieListUseCase {
-//    func getMovieList(pageNumber: Int, endpoint: HomeEndpoint, completion: @escaping ((Movie?, String?) -> Void)) {
-//        NetworkManager.request(
-//            model: Movie.self,
-//            endpoint: endpoint.rawValue,
-//            completion: completion)
-//    }
-
-
-//}
-
-//class HomeManager: MovieListUseCase {
-//    func getMovieList(pageNumber: Int, endpoint: HomeEndpoint, completion: @escaping ((Movie?, String?) -> Void)) {
-//        NetworkManager.request(
-//            model: Movie.self,
-//            endpoint: endpoint.rawValue,
-//            completion: completion)
-//    }
-
-
-//}
+//--------------------------
